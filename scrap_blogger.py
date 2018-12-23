99e448039b2b166da1548a4807ba667f478acc33
Quentin Augé <quentin.auge@gmail.com>
2018-12-23 18:07:06 +0100

    misc changes in scrap_blogger.py

--- scrap_blogger.py
+++ scrap_blogger.py
@@ -20,13 +20,10 @@ class BloggerSpider(scrapy.Spider):
     until there is not article left.
     """
 
-    def __init__(self, last_article_url):
+    def __init__(self, start_article_url):
         super(BloggerSpider).__init__()
-        self.last_article_url = last_article_url
+        self.start_urls = [start_article_url]
         self.folder = 'data/blogger/'
-
-    def start_requests(self):
-        yield Request(self.last_article_url, self.parse)
 
     def parse(self, response):
         url = response.url
@@ -45,19 +42,18 @@ class BloggerSpider(scrapy.Spider):
         filepath = os.path.join(self.folder, filename)
 
         LOGGER.info(f'Saving article to {filepath}')
-        with open(filepath, 'wb') as f:
-            f.write(html)
+        # with open(filepath, 'wb') as f:
+        #     f.write(html)
 
 
 if __name__ == '__main__':
-
-    # File urls.yaml is not intentionally not committed in order to hide the identity
+    # File `urls.yaml` is intentionally not committed in order to hide the identity
     # of the scraped blogs
     with open('urls.yaml') as f:
         urls = yaml.load(f.read())
-        blogger_last_article_url = urls['blogger']
+        start_article_url = urls['blogger']
 
     crawler = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
                               'DOWNLOAD_DELAY': 10})
-    crawler.crawl(BloggerSpider, blogger_last_article_url)
+    crawler.crawl(BloggerSpider, start_article_url)
     crawler.start()
