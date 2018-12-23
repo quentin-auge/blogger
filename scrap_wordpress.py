d19c845537795558be63193c0a20907595836495
Quentin Augé <quentin.auge@gmail.com>
2018-12-23 18:07:35 +0100

    fix wordpress scraping strategy
    "previous article" does not work. The link is absent from most articles.
    Use articles index pages instead.

--- scrap_wordpress.py
+++ scrap_wordpress.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import re
 
 import scrapy
 import yaml
@@ -16,32 +17,28 @@ class WordpressSpider(scrapy.Spider):
     """
     Scrap wordpress articles.
 
-    Start with an article, and proceed to the previous article repeatedly
-    until there is not article left.
+    Start with a list of articles index pages, and get all the articles listed in them.
     """
 
-    def __init__(self, last_article_url):
+    def __init__(self, articles_index_urls):
         super(WordpressSpider).__init__()
-        self.last_article_url = last_article_url
+        self.start_urls = articles_index_urls
         self.folder = 'data/wordpress/'
 
-    def start_requests(self):
-        yield Request(self.last_article_url, self.parse)
-
     def parse(self, response):
-        url = response.url
-        html = response.body
-        self.save_article(url, html)
-        yield from self.fetch_previous_article(html)
-
-    def fetch_previous_article(self, html):
-        soup = BeautifulSoup(html, features='lxml')
+        soup = BeautifulSoup(response.body, features='lxml')
 
         for a in soup.find_all('a'):
-            if 'Article précédent' in a.text:
-                yield Request(a['href'], self.parse)
+            href = a['href']
+            if re.search(r'\?p=[0-9]+$', href):
+                LOGGER.info(f'processing {a}')
+                yield Request(href, self.save_article)
 
-    def save_article(self, url, html):
+    def save_article(self, response):
+
+        url = response.url
+        html = response.body
+
         filename = url.split('/')[-1]
         # ?p=12345 -> 12345.html
         filename = filename[3:] + '.html'
@@ -53,13 +50,13 @@ class WordpressSpider(scrapy.Spider):
 
 
 if __name__ == '__main__':
-    # File urls.yaml is not intentionally not committed in order to hide the identity
+    # File `urls.yaml` is intentionally not committed in order to hide the identity
     # of the scraped blogs
     with open('urls.yaml') as f:
         urls = yaml.load(f.read())
-        wordpress_last_article_url = urls['wordpress']
+        articles_index_urls = urls['wordpress']
 
     crawler = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
                               'DOWNLOAD_DELAY': 10})
-    crawler.crawl(WordpressSpider, wordpress_last_article_url)
+    crawler.crawl(WordpressSpider, articles_index_urls)
     crawler.start()
