01c33f142c5548c5bfaf764bef1f8990b831e304
Quentin Augé <quentin.auge@gmail.com>
2019-01-20 15:54:29 +0100

    document text extraction scripts

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-## Datasets
+## Data
 
 The articles of two (french-speaking) travel blogs are downloaded: a blogger (blogspot) blog and a wordpress blog.
 
-URLs are hidden purposefully in order to protect the identity of the downloaded blogs. As a consequence, you won't get full reproducibility here. Sorry.
+*Note*: URLs are hidden purposefully in order to protect the identity of the downloaded blogs. You won't be able to access the data. Sorry.
 
 The goal is to generate text that looks similar to the blogger blog. Since it is too small a dataset to learn from directly (77k words, 400k characters) we'll learn from a larger wordpress blog first (473k words, 2.7M characters).
 
-Retrieving of the datasets is handled by two scripts:
- * [scrap_wordpress.py], which downloads the raw blogger HTML articles to `data/html/wordpress/`
- * [scrap_blogger.py], which downloads the raw wordpress HTML articles to `data/html/blogger/`
+Retrieving and cleaning the datasets is handled by three scripts:
+  * [scrap_wordpress.py] downloads the raw blogger HTML articles.
+  * [scrap_blogger.py] downloads the raw wordpress HTML articles.
+  * [html_to_txt.py] transforms the raw HTML articles to txt articles. It further discards non-relevant characters (see [analyze_vocab.ipynb]) and concatenates the result into one txt file.
--- html_to_txt.py
+++ html_to_txt.py
@@ -2,6 +2,7 @@
 
 import abc
 import logging
+import os
 from glob import glob
 
 from bs4 import BeautifulSoup
@@ -129,15 +130,17 @@ class WordpressTextExtractor(TextExtractor):
         self._remove_divs_by_id(ids)
 
 
-def create_txt_dataset(blog_name):
+def html_to_txt(html_folder, txt_folder, blog_name):
     """
-    Create txt dataset from html files.
+    Extract the text from html articles. One txt file per html file.
 
     Args:
-        blog_name (str): either 'blogger' or 'wordpress'.
+        html_folder (str): the source folder containing the raw HTML articles.
+        txt_folder (str): the destination folder where extracted txt articles end up.
+        blog_name (str): the blog to transform, either 'blogger' or 'wordpress'.
     """
 
-    assert blog_name in ('blogger', 'wordpress')
+    assert blog_name in ('blogger', 'wordpress'), f'invalid blog_name: {blog_name}'
 
     if blog_name == 'blogger':
         cls = BloggerTextExtractor
@@ -146,8 +149,8 @@ def create_txt_dataset(blog_name):
     else:
         raise NotImplementedError
 
-    html_filepaths = glob(f'data/html/{blog_name}/*.html')
-    LOGGER.info(f'processing {blog_name} ({len(html_filepaths)} articles)')
+    html_filepaths = glob(os.path.join(html_folder, '*.html'))
+    LOGGER.info(f'Processing {html_folder} ({len(html_filepaths)} articles)')
 
     for html_filepath in html_filepaths:
         text = cls(html_filepath).get_text()
@@ -156,31 +159,72 @@ def create_txt_dataset(blog_name):
             LOGGER.info(f'  Empty article {html_filepath}; skipping')
             continue
 
-        txt_filepath = html_filepath.replace('html', 'txt')
+        html_filename = os.path.basename(html_filepath)
+        txt_filename = html_filename.replace('html', 'txt')
+        txt_filepath = os.path.join(txt_folder, txt_filename)
         LOGGER.info(f'  {html_filepath} -> {txt_filepath}')
 
         with open(txt_filepath, 'w') as f:
             f.write(text)
 
 
-def create_one_txt_dataset(blog_name):
+def normalize_txt(txt):
     """
-    Create one txt dataset from multiple txt files.
+    Convert some "exotic" characters to known ones.
+    """
+
+    # Non-breaking spaces -> regular spaces
+    txt = txt.replace('\xa0', ' ')
+
+    # Double quotes
+    double_quotes_chars = '“”»«'
+    for double_quotes_char in double_quotes_chars:
+        txt = txt.replace(double_quotes_char, '"')
+
+    # Single quotes
+    single_quote_chars = '‘`´’'
+    for single_quote_char in single_quote_chars:
+        txt = txt.replace(single_quote_char, "'")
+
+    # Triple dots
+    txt = txt.replace('…', '...')
+
+    # Hyphens
+    hyphen_chars = '–—'
+    for hyphen_char in hyphen_chars:
+        txt = txt.replace(hyphen_char, '-')
+
+    return txt
+
+
+def restrict_to_vocab(txt, vocab):
+    """
+    Remove characters that do not belong the a vocabulary.
+    """
+    txt = ''.join(char for char in txt if char in vocab)
+    return txt
+
+
+def txt_to_one_txt(txt_folder, one_txt_filepath, vocab):
+    """
+    Restrict the set of characters used in txt articles and concatenate the result into one big
+    txt file.
 
     Args:
-        blog_name (str): either 'blogger' or 'wordpress'.
+        txt_folder (str): the source folder containing the txt articles.
+        one_txt_filepath (str): the filepath to the output txt file.
+        vocab (str): the set of characters to restrict the articles to.
     """
 
-    assert blog_name in ('blogger', 'wordpress')
-
-    filepaths = glob(f'data/txt/{blog_name}/*.txt')
+    filepaths = glob(os.path.join(txt_folder, '*.txt'))
 
     articles = []
     for filepath in filepaths:
         with open(filepath) as f:
-            articles.append(f.read())
-
-    one_txt_filepath = f'data/one_txt/{blog_name}.txt'
+            article = f.read()
+            article = normalize_txt(article)
+            article = restrict_to_vocab(article, vocab)
+            articles.append(article)
 
     LOGGER.info(f'Writing {one_txt_filepath}')
 
@@ -189,21 +233,17 @@ def create_one_txt_dataset(blog_name):
 
 
 if __name__ == '__main__':
-
     logging.basicConfig(level=logging.INFO)
 
-    # extractor = BloggerTextExtractor(glob('data/html/blogger/*')[0])
-    # extractor = WordpressTextExtractor(glob('data/html/wordpress/*')[0])
-    #
-    # extractor.trim()
-    #
-    # with open('trimmed.html', 'w') as f:
-    #     f.write(extractor.get_html())
-    #
-    # print(extractor.get_text())
+    blog_names = ('wordpress', 'blogger')
 
-    #create_txt_dataset('blogger')
-    #create_txt_dataset('wordpress')
+    # Raw HTML articles -> txt articles
+    for blog_name in blog_names:
+        html_to_txt(f'data/html/{blog_name}/', f'data/txt/{blog_name}/', blog_name)
 
-    create_one_txt_dataset('blogger')
-    create_one_txt_dataset('wordpress')
+    vocab = ' !"$%\'()+,-./0123456789:;=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    vocab += '_abcdefghijklmnopqrstuvwxyz~°àâçèéêëîïôùûœо€'
+
+    # txt articles -> one clean txt
+    for blog_name in blog_names:
+        txt_to_one_txt(f'data/txt/{blog_name}/', f'data/one_txt/{blog_name}.txt', vocab)
